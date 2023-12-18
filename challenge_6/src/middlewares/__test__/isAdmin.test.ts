3976fd8d89e7f5a030d0f44e1c60614fde8754bf
aldilukmn <aldilukman99@gmail.com>
2023-12-18 22:43:24 +0700

    update: unit test on middleware user/isAdmin

---
+++ challenge_6/src/middlewares/__test__/isAdmin.test.ts
@@ -0,0 +1,134 @@
+import jwt from 'jsonwebtoken'
+import type { Request, Response, NextFunction } from 'express'
+import UserMiddleware from '../user'
+import type DefaultResponse from '../../models/dto/response'
+
+const createResError = (error: string): DefaultResponse => {
+  return {
+    status: {
+      code: error.includes('admin') ? 403 : 401,
+      response: 'fail',
+      message: error
+    }
+  }
+}
+
+describe('JWT Verification for Admin', () => {
+  it('should verify valid token', async () => {
+    const payload = { user: 'admin', role: 'admin' }
+    const secretKey = process.env.SECRET_KEY ?? 'rahasia'
+    const token = jwt.sign(payload, secretKey)
+
+    const req: Partial<Request> = {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    }
+
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    const next: NextFunction = jest.fn()
+
+    await UserMiddleware.isAdmin(req as Request, res as Response, next)
+
+    expect(next).toHaveBeenCalled()
+    expect(res.status).not.toHaveBeenCalled()
+  })
+
+  it('should handle missing token', async () => {
+    const req: Partial<Request> = {
+      headers: {}
+    }
+
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    const next: NextFunction = jest.fn()
+
+    await UserMiddleware.isAdmin(req as Request, res as Response, next)
+
+    expect(next).not.toHaveBeenCalled()
+    expect(res.status).toHaveBeenCalledWith(401)
+    expect(res.json).toHaveBeenCalledWith(createResError('Please login first!'))
+  })
+
+  it('should handle wrong token format', async () => {
+    const payload = { user: 'admin', role: 'admin' }
+    const secretKey = process.env.SECRET_KEY ?? 'rahasia'
+    const token = jwt.sign(payload, secretKey)
+
+    const req: Partial<Request> = {
+      headers: {
+        authorization: token
+      }
+    }
+
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    const next: NextFunction = jest.fn()
+
+    await UserMiddleware.isAdmin(req as Request, res as Response, next)
+
+    expect(next).not.toHaveBeenCalled()
+    expect(res.status).toHaveBeenCalledWith(401)
+    expect(res.json).toHaveBeenCalledWith(createResError('Wrong format token!'))
+  })
+
+  it('should verify invalid token', async () => {
+    const payload = { user: 'admin', role: 'admin' }
+    const secretKey = 'difference-token'
+    const token = jwt.sign(payload, secretKey)
+
+    const req: Partial<Request> = {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    }
+
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    const next: NextFunction = jest.fn()
+
+    await UserMiddleware.isAdmin(req as Request, res as Response, next)
+
+    expect(next).not.toHaveBeenCalled()
+    expect(res.status).toHaveBeenCalledWith(401)
+    expect(res.json).toHaveBeenCalledWith(createResError('Invalid token!'))
+  })
+
+  it('should verify non-admin', async () => {
+    const payload = { user: 'admin', role: 'non-admin' }
+    const secretKey = process.env.SECRET_KEY ?? 'rahasia'
+    const token = jwt.sign(payload, secretKey)
+
+    const req: Partial<Request> = {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    }
+
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    const next: NextFunction = jest.fn()
+
+    await UserMiddleware.isAdmin(req as Request, res as Response, next)
+
+    expect(next).not.toHaveBeenCalled()
+    expect(res.status).toHaveBeenCalledWith(403)
+    expect(res.json).toHaveBeenCalledWith(createResError('Access denied for non-admin users!'))
+  })
+})
---
+++ challenge_6/src/middlewares/__test__/verifyToken.test.ts
@@ -0,0 +1,107 @@
+import jwt from 'jsonwebtoken'
+import type { Request, Response, NextFunction } from 'express'
+import UserMiddleware from '../user'
+import type DefaultResponse from '../../models/dto/response'
+
+const createResError = (error: string): DefaultResponse => {
+  return {
+    status: {
+      code: 401,
+      response: 'fail',
+      message: error
+    }
+  }
+}
+
+describe('JWT Verification', () => {
+  it('should verify valid token', async () => {
+    const payload = { user: 'admin', role: 'admin' }
+    const secretKey = process.env.SECRET_KEY ?? 'rahasia'
+    const token = jwt.sign(payload, secretKey)
+
+    const req: Partial<Request> = {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    }
+
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    const next: NextFunction = jest.fn()
+
+    await UserMiddleware.verifyToken(req, res as Response, next)
+
+    expect(next).toHaveBeenCalled()
+    expect(res.status).not.toHaveBeenCalled()
+  })
+
+  it('should handle missing token', async () => {
+    const req: Partial<Request> = {
+      headers: {}
+    }
+
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    const next: NextFunction = jest.fn()
+
+    await UserMiddleware.verifyToken(req, res as Response, next)
+
+    expect(next).not.toHaveBeenCalled()
+    expect(res.status).toHaveBeenCalledWith(401)
+    expect(res.json).toHaveBeenCalledWith(createResError('Please login first!'))
+  })
+
+  it('should handle wrong token format', async () => {
+    const payload = { user: 'admin', role: 'admin' }
+    const secretKey = process.env.SECRET_KEY ?? 'rahasia'
+    const token = jwt.sign(payload, secretKey)
+
+    const req: Partial<Request> = {
+      headers: {
+        authorization: token
+      }
+    }
+
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    const next: NextFunction = jest.fn()
+
+    await UserMiddleware.verifyToken(req, res as Response, next)
+
+    expect(next).not.toHaveBeenCalled()
+    expect(res.json).toHaveBeenCalledWith(createResError('Wrong format token!'))
+  })
+
+  it('should verify invalid token', async () => {
+    const payload = { user: 'admin', role: 'admin' }
+    const secretKey = 'difference-token'
+    const token = jwt.sign(payload, secretKey)
+
+    const req: Partial<Request> = {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    }
+
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    const next: NextFunction = jest.fn()
+
+    await UserMiddleware.verifyToken(req, res as Response, next)
+
+    expect(next).not.toHaveBeenCalled()
+    expect(res.json).toHaveBeenCalledWith(createResError('Invalid token!'))
+  })
+})
